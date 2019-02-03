bc8b1e8dd746e0c613228dbe1f66d65edb0b32ef
alexlimon <alex.limon@mavs.uta.edu>
2019-02-03 15:28:58 -0600

    adding new branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,17 +6,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome to my website! This is an obvious work in progress. Meanwhile checkout my LinkedIn below:
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.linkedin.com/in/alejandrolimonnaranjo/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+          Alex's LinkedIn
           </a>
         </header>
       </div>
