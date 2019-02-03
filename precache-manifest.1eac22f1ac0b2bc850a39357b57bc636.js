b8420ffdfdaff5dd19b095e5f48f9b6927ea5d96
alexlimon <alex.limon@mavs.uta.edu>
2019-02-03 15:41:09 -0600

    Updates

---
+++ precache-manifest.1eac22f1ac0b2bc850a39357b57bc636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ba86f2a6151d9236a2b",
+    "url": "/static/js/main.2ba86f2a.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "2ba86f2a6151d9236a2b",
+    "url": "/static/css/main.b00153e2.chunk.css"
+  },
+  {
+    "revision": "af7299b00eedfed30446e75dda3817cd",
+    "url": "/index.html"
+  }
+];
